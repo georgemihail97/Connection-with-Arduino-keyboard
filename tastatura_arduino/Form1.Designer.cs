be5f2f18f19713370cd63a203c0201bf11c4be75
unknown <batiegeorge@yahoo.com>
2020-12-10 18:34:50 +0200

    First commit

---
+++ tastatura_arduino/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace tastatura_arduino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.afisareTxt = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // afisareTxt
+            // 
+            this.afisareTxt.Location = new System.Drawing.Point(29, 47);
+            this.afisareTxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.afisareTxt.Multiline = true;
+            this.afisareTxt.Name = "afisareTxt";
+            this.afisareTxt.Size = new System.Drawing.Size(499, 29);
+            this.afisareTxt.TabIndex = 0;
+            this.afisareTxt.TextChanged += new System.EventHandler(this.afisareTxt_TextChanged);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.afisareTxt);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Laborator 6";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox afisareTxt;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ tastatura_arduino/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tastatura_arduino
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void citestePort()
+        {
+            try
+            {
+                // Deschide portul serial pentru a putea citi datele
+                serialPort1.Open();
+                // Deoarece nu se cunoaste momentul in care utilizatorul va genera evenimentul de apasare a tastei,
+                // se va implementa un fir de executie separat care va permite citirea continua, fara sa afecteze interfata grafica
+                // adica fara sa blocheze (inghete) interfata cu utilizatorul
+                serialPort1.DataReceived += new SerialDataReceivedEventHandler(serialPortData);
+
+            }
+            catch (Exception ex)
+            {
+                afisareTxt.Text = "Nu se poate deschide portul";
+            }
+        }
+
+        public void AfiseazaText(string text)
+        {
+            // Se invoca un nou fir de executie pentru afisarea in textBox
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(AfiseazaText), new object[] { text });
+                return;
+            }
+            afisareTxt.Text = text;
+        }
+
+
+        void serialPortData(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                // Citeste datele pe portul serial
+                string res = serialPort1.ReadLine();
+                // Deoarece citirea se realizeaza intr-un fir de executie, scrierea in textBox in mod direct nu este posibila
+                // Din aceste considerente se invoca o alta metoda care permite scrierea intr-un alt fir de executie (Thread)
+                AfiseazaText(res);
+            }
+            catch (Exception ex)
+            {
+                AfiseazaText("eroare!");
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            // Atunci cand utilizatorul inchide aplicatia, trebuie inchis si portul serial
+            // In caz contrar, la o rulare ulterioara, atunci cand va incerca sa deschida aplicatia va rezulta in eroare, deoarece portul este deja deschis de la executia anterioara
+            if (serialPort1.IsOpen)
+                serialPort1.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Apelez metoda care implementeaza comunicatia pe portul serial
+            citestePort();
+        }
+
+        private void afisareTxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
